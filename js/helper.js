f8096141023c51747b8b86c183939737ff7260ee
cameronwp <cameron.w.pittman@gmail.com>
2014-07-18 16:07:20 -0700

    Finalized project workflow

---
+++ js/helper.js
@@ -0,0 +1,42 @@
+var HTMLheaderName = "<h1>%data%</h1>";
+var HTMLheaderRole = "<span>%data%</span><hr/>";
+
+var HTMLmobile = "<li class='flex-item'><span class='orange-text'>mobile</span><span class='white-text'>%data%</span></li>";
+var HTMLemail = "<li class='flex-item'><span class='orange-text'>email</span><span class='white-text'>%data%</span></li>";
+var HTMLtwitter = "<li class='flex-item'><span class='orange-text'>twitter</span><span class='white-text'>%data%</span></li>";
+var HTMLgithub = "<li class='flex-item'><span class='orange-text'>github</span><span class='white-text'>%data%</span></li>";
+var HTMLblog = "<li class='flex-item'><span class='orange-text'>blog</span><span class='white-text'>%data%</span></li>";
+var HTMLlocation = "<li class='flex-item'><span class='orange-text'>location</span><span class='white-text'>%data%</span></li>"; // current city?
+var HTMLbioPic = "<img src='%data%' class='biopic'>";
+var HTMLWelcomeMsg = "<span class='welcome-message'>%data%</span>"
+
+var HTMLworkEmployer = "<div class='work-entry'><a href='#'>%data%";
+var HTMLworkTitle = " - %data%</a>";
+var HTMLworkDates = "<div class='date-text'>%data%</div>";
+var HTMLworkDescription = "<p>%data%</p>";
+
+var HTMLprojectStart = "<div id='projects'><h2>Projects</h2>";
+var HTMLprojectEnd = "</div></div>";
+
+var HTMLprojectTitle = "<div class='project-entry'><a href='#'>%data%</a>";
+var HTMLprojectDates = "<div class='date-text'>%data%</div>";
+var HTMLprojectDescription = "<p>%data%</p>";
+var HTMLprojectImage = "<img src='%data%'>";
+var HTMLprojectImageEnd = "<br/>";
+
+var HTMLeducationStart = "<div id='education' class='gray'><h2>Educational Experience</h2>";
+var HTMLeducationEnd = "</div>";
+
+var HTMLonlineStart = "<h3>Online Courses</h3>";
+
+var HTMLschoolName = "<div class='education-entry'><a href='#'>%data%";
+var HTMLschoolDegree = " -- %data%</a>";
+var HTMLschoolDates = "<div class='date-text'>%data%</div>";
+var HTMLschoolDescription = "<p>%data%</p></div>"
+
+var HTMLonlineTitle = "<a href='#'>%data%";
+var HTMLonlineSchool = " - %data%</a>";
+var HTMLonlineDescription = "<p>%data%</p></div>";
+
+var HTMLcontactStart = "<div id='letsConnect' class='dark-gray'><h2 class='orange center-text'>Let's Connect</h2>";
+var HTMLcontactEnd = "</div>";
---
+++ js/resumeBuilder2.js
@@ -0,0 +1,112 @@
+var bioInformation = {
+  "name" : "John Doe",
+  "role": "Web Developer",
+  "mobile": "650-555-5555",
+  "email": "john@example.com",
+  "github": "johndoe",
+  "twitter": "johndoe",
+  "currentCity": "San Francisco",
+  "welcomeMessage": "lorem ipsum dolor sit amet etc etc etc.",
+  "bioPic": "http://img2.wikia.nocookie.net/__cb20110606042505/es.futurama/images/0/0b/55692752-Philip_J_Fry.jpg"
+}
+
+var education = {
+  "schools": [
+    {
+      "name": "Nova Southeastern University",
+      "city": "Fort Lauderdale, FL",
+      "degree": "Masters",
+      "major": ["CS"],
+      "graduationDate": 2013,
+      "url": "http://example.com"
+    },
+    {
+      "name": "Eckerd College",
+      "city": "Saint Petersburg, FL",
+      "degree": "BA",
+      "major": ["CS"],
+      "graduationDate": 2003,
+      "url": "http://example.com"
+    }
+  ],
+  "onlineCourses": [
+    {
+      "courseTitle": "Introduction to Machine Learning",
+      "name" : "Udacity",
+      "graduationDate": 2014,
+      "url": "http://example.com"
+    }
+  ]
+}
+
+var work = {
+  "jobs": [
+    {
+      "employer": "Planet Express",
+      "title": "Delivery Boy",
+      "dates": "January 3000 - Future",
+      "description": "Who moved my cheese cheesy feet cauliflower cheese. Queso taleggio when the cheese comes out everybody's happy airedale ricotta cheese and wine paneer camembert de normandie. Swiss mozzarella cheese slices feta fromage frais airedale swiss cheesecake. Hard cheese blue castello halloumi parmesan say cheese stinking bishop jarlsberg."
+    },
+    {
+      "employer": "Panucci's Pizza",
+      "title": "Delivery Boy",
+      "dates": "1998 - December 31, 1999",
+      "description": "Who moved my cheese cheesy feet cauliflower cheese. Queso taleggio when the cheese comes out everybody's happy airedale ricotta cheese and wine paneer camembert de normandie. Swiss mozzarella cheese slices feta fromage frais airedale swiss cheesecake. Hard cheese blue castello halloumi parmesan say cheese stinking bishop jarlsberg."
+    }
+  ]
+}
+
+var projects = {
+  "projects": [
+    {
+      "title": "Sample Project 1",
+      "date": "2014",
+      "description": "Who moved my cheese cheesy feet cauliflower cheese. Queso taleggio when the cheese comes out everybody's happy airedale ricotta cheese and wine paneer camembert de normandie. Swiss mozzarella cheese slices feta fromage frais airedale swiss cheesecake. Hard cheese blue castello halloumi parmesan say cheese stinking bishop jarlsberg.",
+      "images": [
+        "https://lh3.ggpht.com/23-sqOpOGqF06YX3BwIYPIXLX_Ma_clLXySKEHlphqlxr2l-PPbC80U8SjDi96KTWbNjKfY2Pdq_gyFK9A=s300#w=1757&h=1080",
+        "https://lh3.ggpht.com/23-sqOpOGqF06YX3BwIYPIXLX_Ma_clLXySKEHlphqlxr2l-PPbC80U8SjDi96KTWbNjKfY2Pdq_gyFK9A=s300#w=1757&h=1080"
+      ]   
+    }
+  ]
+}
+
+var formattedRole = HTMLheaderRole.replace("%data%", bioInformation.role)
+$("#header").prepend(formattedRole);
+
+var formattedName = HTMLheaderName.replace("%data%", bioInformation.name);
+$("#header").prepend(formattedName);
+
+var formattedEmail = HTMLemail.replace("%data%",bioInformation.email);
+$("#topContacts").append(formattedEmail);
+
+var formattedMobile = HTMLmobile.replace("%data%",bioInformation.mobile);
+$("#topContacts").append(formattedMobile);
+
+var formattedPic = HTMLbioPic.replace("%data%", bioInformation.bioPic);
+$("#header").append(formattedPic);
+
+var formattedDescription = HTMLWelcomeMsg.replace("%data%", bioInformation.welcomeMessage);
+$("#header").append(formattedDescription);
+
+
+var 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
