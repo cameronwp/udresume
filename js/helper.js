8eeabf126ad02330f5dc522ccda3b81d30d9c7ed
James Williams <James.L.Williams@gmail.com>
2014-07-21 14:39:53 -0700

    Added style to Skills H3 tag.

--- js/helper.js
+++ js/helper.js
@@ -11,7 +11,7 @@ var HTMLlocation = "<li class='flex-item'><span class='orange-text'>location</sp
 var HTMLbioPic = "<img src='%data%' class='biopic'>";
 var HTMLWelcomeMsg = "<span class='welcome-message'>%data%</span>";
 
-var HTMLskillsStart = "<h3>Skills at a Glance:</h3><ul id='skills' class='flex-box'></ul>";
+var HTMLskillsStart = "<h3 id='skillsH3'>Skills at a Glance:</h3><ul id='skills' class='flex-box'></ul>";
 var HTMLskills = "<li class='flex-item'><span class='white-text'>%data%</span></li>";
 
 var HTMLworkStart = "<div class='work-entry'></div>";
