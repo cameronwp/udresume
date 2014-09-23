8c13378b70c4f8a448aebe13c97583bd6fc937ca
cameronwp <cameron.w.pittman@gmail.com>
2014-09-22 23:08:47 -0700

    prepping for students

--- js/helper.js
+++ js/helper.js
@@ -120,7 +120,7 @@ function initializeMap() {     // called when page is loaded
     });
 
     google.maps.event.addListener(marker, 'click', function() {
-      infoWindow.open(map, marker);                     // probably going to cut this line for the student quiz
+      // infoWindow.open(map, marker);                     // probably going to cut this line for the student quiz
    
     });
 
